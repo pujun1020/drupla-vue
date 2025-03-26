49ce5935437f710b501973c53a93385fba3ce602
pujun1020 <136682149+pujun1020@users.noreply.github.com>
2025-03-26 17:46:27 +0800

    提交变更分支

---
+++ src/views/Test/Test.vue
@@ -0,0 +1,27 @@
+
+<script lang="ts">
+export default {
+  name: 'Testuuu'
+}
+</script>
+<script setup lang="ts">
+
+import { onActivated, onDeactivated } from 'vue'
+
+onActivated(() => {
+  // 调用时机为首次挂载
+  // 以及每次从缓存中被重新插入时
+  console.log("🚀 ~ onActivated ~ 调用时机为首次挂载:")
+})
+
+
+onDeactivated(() => {
+  // 在从 DOM 上移除、进入缓存
+  // 以及组件卸载时调用
+  console.log("🚀 ~ onDeactivated ~ DOM:")
+})
+ 
+</script>
+<template>
+    <input type="text" style="margin-top: 300px;">
+</template>
